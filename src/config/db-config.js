ec9e3f0571e5469585b656934a0fd518ba292afb
Arpita Rai <arpitarai@Arpitas-MacBook-Pro.local>
2024-02-12 22:11:36 -0500

    db change

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -3,23 +3,10 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-console.log(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD);
-// const sequelize = new Sequelize('nodeexpressmysql', 'root', 'rootroot', {
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
+
 const sequelize = new Sequelize(process.env.DB_DATABASE || 'nodeexpressmysql', process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
-        
-    //    // Option to create the database if it doesn't exist
-    // define: {
-    //     charset: 'utf8mb4',
-    //     collate: 'utf8mb4_unicode_ci',
-    //     timestamps: false,
-    // },
-    // // Option to create the database if it doesn't exist
-    // sync: { force: process.env.DB_FORCE_SYNC === 'true' },
 });
 
 // Sync the database and create tables if they don't exist
