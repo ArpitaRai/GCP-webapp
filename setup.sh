ba3df073e4bac868cd10be254260671621845267
Arpita Rai <42668979+ArpitaRai@users.noreply.github.com>
2024-02-26 20:46:59 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,13 +72,13 @@ sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service || { echo "Failed
 # sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
 # sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
 
-echo "================================================================="
-echo "Run the application"
-echo "================================================================="
+# echo "================================================================="
+# echo "Run the application"
+# echo "================================================================="
 
-sudo systemctl daemon-reload
-sudo systemctl enable webapp.service
-sudo systemctl start webapp.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable webapp.service
+# sudo systemctl start webapp.service
 
 echo "=======================ALL DONE==================================="
 
