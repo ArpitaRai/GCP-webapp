d8a26453b15715991d874a4b90e7f9ee7bc9a946
Arpita Rai <42668979+ArpitaRai@users.noreply.github.com>
2024-02-26 20:27:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,12 +65,12 @@ echo "================================================================="
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service || { echo "Failed to move webapp.service . Exiting."; exit 1; }
 
-echo "================================================================="
-echo "Creating user and changing directory ownership"
-echo "================================================================="
-sudo adduser csye6225 --shell /usr/sbin/nologin || { echo "Failed to add csye6225 user. Exiting."; exit 1; }
-sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
-sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
+# echo "================================================================="
+# echo "Creating user and changing directory ownership"
+# echo "================================================================="
+# sudo adduser csye6225 --shell /usr/sbin/nologin || { echo "Failed to add csye6225 user. Exiting."; exit 1; }
+# sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
+# sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
 
 echo "================================================================="
 echo "Run the application"
