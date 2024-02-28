b4a0c5ef4f5ae44ef217a42ddcfaa1be588cfe75
Arpita Rai <arpitarai@Arpitas-MacBook-Pro.local>
2024-02-27 22:53:40 -0500

    check for script filr

--- setup.sh
+++ setup.sh
@@ -65,20 +65,20 @@ echo "================================================================="
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service || { echo "Failed to move webapp.service . Exiting."; exit 1; }
 
-# echo "================================================================="
-# echo "Creating user and changing directory ownership"
-# echo "================================================================="
-# sudo adduser csye6225 --shell /usr/sbin/nologin || { echo "Failed to add csye6225 user. Exiting."; exit 1; }
-# sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
-# sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
+echo "================================================================="
+echo "Creating user and changing directory ownership"
+echo "================================================================="
+sudo adduser csye6225 --shell /usr/sbin/nologin || { echo "Failed to add csye6225 user. Exiting."; exit 1; }
+sudo chown -R csye6225:csye6225 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
+sudo chmod -R 744 /opt/csye6225dir || { echo "Failed to change directory permissions. Exiting."; exit 1; }
 
-# echo "================================================================="
-# echo "Run the application"
-# echo "================================================================="
+echo "================================================================="
+echo "Run the application"
+echo "================================================================="
 
-# sudo systemctl daemon-reload
-# sudo systemctl enable webapp.service
-# sudo systemctl start webapp.service
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
 
 echo "=======================ALL DONE==================================="
 
